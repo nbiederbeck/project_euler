1f1f3d7b761c012b63fe5d0564447daae7223cc5
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-12-05 23:15:43 +0100

    Restructure file tree

--- python/telegramupdater.py
+++
@@ -1,9 +0,0 @@
-import telepot
-token = '394103686:AAHlJUxbqtfCm92XklUlmU2jTRU2uTqvwUI'
-chatid = 322086570
-bot = telepot.Bot(token)
-def message(text):
-    if type(text)==str:
-       bot.sendMessage(chat_id=chatid, text=text)
-    else:
-        print('WARNING: message must be string-type!')
